fd4cc9de00c09b027ce6356a605a536307b06f0e
Alan Grow <alangrow@gmail.com>
2010-12-06 01:59:17 -0500

    fix off by one range in problem1

--- problem1.scm
+++ problem1.scm
@@ -1,4 +1,4 @@
 (define (range a b) (if (<= a b) (cons a (range (+ 1 a) b)) '() ) )
 (define (sumlist l) (if (eq? l '()) 0 (+ (car l) (sumlist (cdr l)))))
 (define (isdiv3or5 x) (or (= 0 (modulo x 3)) (= 0 (modulo x 5))))
-(display (sumlist (map (lambda (x) (if (isdiv3or5 x) x 0)) (range 1 1000))))
+(display (sumlist (map (lambda (x) (if (isdiv3or5 x) x 0)) (range 1 999))))
